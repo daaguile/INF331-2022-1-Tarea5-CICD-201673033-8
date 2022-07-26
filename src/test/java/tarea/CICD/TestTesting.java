39ece604b4fa61a1ae0c1eab636b9fdb10802990
daaguile <diego.aguileraa@sansano.usm.cl>
2022-07-26 19:38:08 -0400

    First commit

---
+++ src/test/java/tarea/CICD/TestTesting.java
@@ -0,0 +1,54 @@
+package tarea.CICD;
+
+import static org.junit.Assert.*;
+
+import java.time.Duration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestTesting {
+	
+	private WebDriver driver;
+	private Duration wait;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromeDriver/chromedriver.exe");
+		ChromeOptions o = new ChromeOptions();
+		o.addArguments("--incognito");
+		driver = new ChromeDriver(o);
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		
+		wait = Duration.ofSeconds(10);
+		
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		
+		WebElement searchbox = driver.findElement(By.name("q"));
+		searchbox.clear();
+		searchbox.sendKeys("Probando Selenium");
+		searchbox.submit();
+		
+		driver.manage().timeouts().implicitlyWait(wait);
+		
+		assertEquals("Probando Selenium - Buscar con Google", driver.getTitle());
+	}
+
+}
